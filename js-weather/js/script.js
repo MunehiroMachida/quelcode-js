813e3847f02d4a02088e9c4017850f9bc2f111c6
MUnehiroMachida <life1231@outlook.jp>
2020-10-27 11:48:58 +0900

    letからconstに変更

--- js-weather/js/script.js
+++ js-weather/js/script.js
@@ -1,19 +1,19 @@
-let xhr = new XMLHttpRequest();
+const xhr = new XMLHttpRequest();
 
-let WeatherMapApi = 'http://api.openweathermap.org/data/2.5/weather?q=';
-let ApiLangJa = '&units=metric&lang=ja';
-let AppIdUrl = '&appid=4b5774e9f3d2a07b84f0f2f88e486224';
+const WeatherMapApi = 'http://api.openweathermap.org/data/2.5/weather?q=';
+const ApiLangJa = '&units=metric&lang=ja';
+const AppIdUrl = '&appid=4b5774e9f3d2a07b84f0f2f88e486224';
 
 const todaysWeather = (response) => {
-    let obj = JSON.parse(response);
-    let weather = obj.weather[0].description;
-    let city = obj.name;
+    const obj = JSON.parse(response);
+    const weather = obj.weather[0].description;
+    const city = obj.name;
     document.getElementById('CityName').innerHTML = "現在の" + city + "の天気は" + weather;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     //デフォルトロンドン
-    let LondonUrl = WeatherMapApi + "London" + ApiLangJa + AppIdUrl;
+    const LondonUrl = WeatherMapApi + "London" + ApiLangJa + AppIdUrl;
     xhr.open('GET', LondonUrl, true);
     xhr.send();
     //通信ステータスが変わったら実行される関数
@@ -25,8 +25,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     // id、weatherが変わったら↓
     document.getElementById('weather').addEventListener('change', function () {
-        let CityName = this.value;
-        let requestUrl = WeatherMapApi + CityName + ApiLangJa + AppIdUrl;
+        const CityName = this.value;
+        const requestUrl = WeatherMapApi + CityName + ApiLangJa + AppIdUrl;
         xhr.open('GET', requestUrl, true);
         xhr.send();
         //通信ステータスが変わったら実行される関数
