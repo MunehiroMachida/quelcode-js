7b7ebe1a0e37106b1ad81e319dc472bbba74a6cb
MUnehiroMachida <life1231@outlook.jp>
2020-10-27 10:57:57 +0900

    jsファイル日本語に変換するif文削除

--- js-weather/js/script.js
+++ js-weather/js/script.js
@@ -33,36 +33,6 @@ document.addEventListener('DOMContentLoaded', function () {
 function TodaysWeather(response) {
     let obj = JSON.parse(response);
     let weather = obj.weather[0].description;
-    // パラメータをjaにすると変な日本語になっていたので再代入
-    if (weather === "broken clouds" || weather === "scattered clouds" || weather === "overcast clouds") {
-        weather = '曇り';
-    } else if (weather === "clear sky") {
-        weather = '快晴';
-    } else if (weather === "few clouds") {
-        weather = '晴れ';
-    } else if (weather === "shower rain") {
-        weather = '小雨';
-    } else if (weather === "rain") {
-        weather = '雨';
-    } else if (weather === "thunderstorm") {
-        weather = '雷雨';
-    } else if (weather === "snow") {
-        weather = '雪';
-    } else if (weather === "mist") {
-        weather = '霧';
-    }
     let city = obj.name;
-    // 都市名を日本語に変更
-    if (city === 'London') {
-        city = 'ロンドン'
-    } else if (city === 'Tokyo') {
-        city = '東京'
-    } else if (city === 'Paris') {
-        city = 'パリ'
-    } else if (city === 'New York') {
-        city = 'ニューヨーク'
-    } else if (city === 'Indonesia') {
-        city = 'インドネシア'
-    }
     document.getElementById('CityName').innerHTML = "現在の" + city + "の天気は" + weather;
 }
