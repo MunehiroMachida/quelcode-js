b40e55e010b0356a08d905ab607b90890555d7cf
MUnehiroMachida <life1231@outlook.jp>
2020-10-27 19:44:04 +0900

    変数名修正

--- js-weather/js/script.js
+++ js-weather/js/script.js
@@ -1,38 +1,38 @@
 const xhr = new XMLHttpRequest();
 
-const WeatherMapApi = 'http://api.openweathermap.org/data/2.5/weather?q=';
-const ApiLangJa = '&units=metric&lang=ja';
-const AppIdUrl = '&appid=4b5774e9f3d2a07b84f0f2f88e486224';
+const weatherMapApi = 'http://api.openweathermap.org/data/2.5/weather?q=';
+const apiLangJa = '&units=metric&lang=ja';
+const appIdUrl = '&appid=4b5774e9f3d2a07b84f0f2f88e486224';
 
 const todaysWeather = (response) => {
     const obj = JSON.parse(response);
     const weather = obj.weather[0].description;
     const city = obj.name;
     document.getElementById('CityName').innerHTML = "現在の" + city + "の天気は" + weather;
-}
+};
 
 document.addEventListener('DOMContentLoaded', () => {
     //デフォルトロンドン
-    const LondonUrl = WeatherMapApi + "London" + ApiLangJa + AppIdUrl;
+    const LondonUrl = weatherMapApi + "London" + apiLangJa + appIdUrl;
     xhr.open('GET', LondonUrl, true);
     xhr.send();
     //通信ステータスが変わったら実行される関数
     xhr.onreadystatechange = () => {
         //通信が完了したらShowtodaysWeather関数を実行
-        if (xhr.readyState == 4) {
+        if (xhr.readyState === 4) {
             todaysWeather(xhr.responseText);
         }
     }
     // id、weatherが変わったら↓
     document.getElementById('weather').addEventListener('change', (event) => {
         const CityName = event.target.value;
-        const requestUrl = WeatherMapApi + CityName + ApiLangJa + AppIdUrl;
+        const requestUrl = weatherMapApi + CityName + apiLangJa + appIdUrl;
         xhr.open('GET', requestUrl, true);
         xhr.send();
         //通信ステータスが変わったら実行される関数
         xhr.onreadystatechange = () => {
             //通信が完了したらShowtodaysWeather関数を実行
-            if (xhr.readyState == 4) {
+            if (xhr.readyState === 4) {
                 todaysWeather(xhr.responseText);
             }
         }
